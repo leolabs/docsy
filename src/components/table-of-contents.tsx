16cba62e1239c61232277ed4c3f639714fdff7b4
Leo Bernard <admin@leolabs.org>
2018-12-10 17:01:59 +0100

    :lipstick: Add nicer styles to ToC

--- src/components/table-of-contents.tsx
+++ src/components/table-of-contents.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { Link, StaticQuery, graphql } from 'gatsby'
+import React from 'react';
+import { Link, StaticQuery, graphql } from 'gatsby';
 import { getTitleFromNode } from '../util/title';
 import title from 'title';
 import classNames from 'classnames';
+
+import '../styles/components/table-of-contents.scss';
 
 interface TreeNode {
   path: string;
@@ -40,70 +42,95 @@ const arrangeIntoTree = (edges: any[]) => {
       const isLast = index === pathParts.length - 1;
 
       if (existingPath.length > 0) {
-        currentLevel = existingPath[0].children
+        if (isLast) {
+          existingPath[0].title = getTitleFromNode(node);
+          existingPath[0].isLinkable = true;
+          existingPath[0].slug = node.fields.slug;
+        }
+
+        currentLevel = existingPath[0].children;
       } else {
         const newPart = {
           path: part,
-          title: isLast ? getTitleFromNode(node) : title(part.replace(/-/g, ' ')),
+          title: isLast
+            ? getTitleFromNode(node)
+            : title(part.replace(/-/g, ' ')),
           slug: node.fields.slug,
           isLinkable: isLast,
           depth: index,
           children: [],
-        }
+        };
 
-        currentLevel.push(newPart)
-        currentLevel = newPart.children
+        currentLevel.push(newPart);
+        currentLevel = newPart.children;
       }
-    })
-  })
+    });
+  });
 
   return tree;
-}
+};
 
-const TocList = ({tree, activeSlug}: any) => {
-  return (<ul>
-    {tree.map((entry: TreeNode, index: number) =>
-      <li
-        key={index}
-        className={classNames({active: entry.isLinkable && entry.slug === activeSlug}, `depth-${entry.depth}`)}
-      >
-        {entry.isLinkable ? <Link to={entry.slug}>{entry.title}</Link> : entry.title}
-        {entry.children && <TocList activeSlug={activeSlug} tree={entry.children} />}
-      </li>
-    )}
-  </ul>)
-}
+const TocList = ({ tree, activeSlug }: any) => {
+  return (
+    <ul>
+      {tree.map((entry: TreeNode, index: number) => (
+        <li
+          key={index}
+          className={classNames(
+            {
+              active: entry.isLinkable && entry.slug === activeSlug,
+              hasChildren: !!entry.children.length,
+              isLinkable: entry.isLinkable,
+            },
+            `depth-${entry.depth}`
+          )}
+        >
+          {entry.isLinkable ? (
+            <Link to={entry.slug}>{entry.title}</Link>
+          ) : (
+            <span>{entry.title}</span>
+          )}
+          {entry.children && (
+            <TocList activeSlug={activeSlug} tree={entry.children} />
+          )}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 interface TableOfContentsProps {
   activeSlug: string;
-};
+}
 
-export default ({activeSlug}: TableOfContentsProps) => (
+export default ({ activeSlug }: TableOfContentsProps) => (
   <StaticQuery
     query={graphql`
-    query TableOfContents {
-      allMarkdownRemark(sort: {fields: fileAbsolutePath, order: ASC}) {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-            }
-            headings {
-              value
+      query TableOfContents {
+        allMarkdownRemark(sort: { fields: fileAbsolutePath, order: ASC }) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+              headings {
+                value
+              }
             }
           }
         }
       }
-    }
-  `}
+    `}
     render={data => (
-      <TocList
-        activeSlug={activeSlug}
-        tree={arrangeIntoTree(data.allMarkdownRemark.edges)}
-      />
+      <nav className="table-of-contents">
+        <TocList
+          activeSlug={activeSlug}
+          tree={arrangeIntoTree(data.allMarkdownRemark.edges)}
+        />
+      </nav>
     )}
   />
-)
+);
