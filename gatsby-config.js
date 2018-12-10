7c2340a814279c0665eac3c45b353a9f87072c89
Leo Bernard <admin@leolabs.org>
2018-12-10 16:58:09 +0100

    :package: Add SASS support

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-typescript',
+    'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
