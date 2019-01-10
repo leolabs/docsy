ca03b3f9b36f60897b9b5f0b16992d3a1c2d03bf
Daniel Wirtz <danielwirtzx@gmail.com>
2019-01-10 14:48:18 +0100

    :wrench: Add disclaimer to side menu

--- config/site-config.js
+++ config/site-config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  title: 'Crisp Studio Playbook',
-  shortTitle: 'Crisp Playbook',
-  description: '',
-  lang: 'en',
-  githubRepo: 'https://github.com/leolabs/docsy',
-  mainBranch: 'master',
-  primaryColor: '#153cff',
-  copyright: 'Made by Crisp Studio',
+  title: "Crisp Studio Playbook",
+  shortTitle: "Crisp Playbook",
+  description: "",
+  lang: "en",
+  githubRepo: "https://github.com/leolabs/docsy",
+  mainBranch: "master",
+  primaryColor: "#153cff",
+  copyright: "Made by Crisp Studio",
   asideMenu: [
     { title: "Homepage", link: "https://crisp.studio" },
     { title: "Instagram", link: "https://instagr.am/crispstudio" },
+    { title: "Disclaimer", link: "https://crisp.studio/pdf/disclaimer.pdf" }
   ]
-}
+};
